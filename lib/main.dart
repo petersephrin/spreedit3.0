987ee33945f24e85a59459b8e329ad2ff93e5b58
Peter Safari <safaripeterw@gmail.com>
2024-07-22 22:18:21 +0300

    feat:open file with file picker;next:gemini going through files and adding them to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -55,17 +56,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void pickFiles() async {
+    FilePickerResult? result =
+        await FilePicker.platform.pickFiles(allowMultiple: true);
+    if (result != null) {
+      print("Files picked");
+    }
   }
 
   @override
@@ -105,20 +101,20 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
+              'Add files to start.',
               style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'Hint: Open multiple files to study a topic.',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        onPressed: pickFiles,
+        tooltip: 'Open files',
+        child: const Icon(Icons.file_open_rounded),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
